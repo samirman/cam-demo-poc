28cc1ea72a87730403e4afe3a3d082491be6648d
chadh <chadh>
2019-03-01 14:57:13 +0000

    fix dclist

--- terraform/custom/camc-sp-python/scripts/test_python_ucd.py
+++ terraform/custom/camc-sp-python/scripts/test_python_ucd.py
@@ -23,7 +23,8 @@ resource_id = data['id']
 returnObj = {}
 returnObj['resource_id'] = resource_id
 returnObj['resource_name'] = resource_name
-returnObj['dclist'] = ['mwdc', 'gtdc']
+#returnObj['dclist'] = ['mwdc', 'gtdc']
+returnObj['dclist'] = json.dumps(['mwdc', 'gtdc'])
 returnObj['all']=json.dumps(data) 
 
 print(json.dumps(returnObj))
