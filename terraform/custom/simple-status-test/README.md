837045111cb355fe4261106d800eedda7ed64479
chadh <chadh>
2019-03-18 12:38:39 +0000

    Add simple status test

---
+++ terraform/custom/simple-status-test/README.md
@@ -0,0 +1,8 @@
+## simple-status-test infrastructure
+
+Created 3/18/19 by admin. for simple-status-test
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ terraform/custom/simple-status-test/main.tf
@@ -0,0 +1,13 @@
+#####################################################################
+##
+##      Created 3/18/19 by admin. for simple-status-test
+##
+#####################################################################
+
+terraform {
+  required_version = "> 0.8.0"
+}
+
+
+resource "null_resource" "null_resource" {
+}
